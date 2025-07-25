19b5dee358e384beda382ac16a90626d04b8668d
Nima Heidari <nima.heidari.n@gmail.com>
2025-07-25 23:00:03 +0330

    feat(post): implement images migration,model with fields

--- database/migrations/2025_07_25_191742_create_images_table.php
+++ database/migrations/2025_07_25_191742_create_images_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('images', function (Blueprint $table) {
             $table->id();
+            $table->string('path');
+            $table->unsignedBigInteger('imageable_id');
+            $table->string('imageable_type');
             $table->timestamps();
         });
     }

49d330d8a94e9fea244ea1cfef19c7ffa0dbfbf9
Nima Heidari <nima.heidari.n@gmail.com>
2025-07-26 15:51:53 +0330

    feat(user): add user (resource) routes with UserController

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::resource('posts', PostController::class);
+Route::resource('users', UserController::class);

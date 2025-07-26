ca212407cc323a407dc7b63e689411a346bd31eb
Nima Heidari <nima.heidari.n@gmail.com>
2025-07-26 15:31:48 +0330

    feat(user): Add baseRules in UserRules

--- app/Rules/UserRules.php
+++ app/Rules/UserRules.php
@@ -5,13 +5,21 @@ use App\Rules\Common\ImageRules;
 
 class UserRules
 {
-    public static function create(): array
+    protected static function baseRules(): array
     {
         return [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'email', 'max:255', 'unique:users,email'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'name' => ['string', 'max:255'],
+            'email' => ['email', 'max:255'],
+            'password' => ['string', 'min:8', 'confirmed'],
             'image' => ImageRules::optionalImage(),
         ];
+    }
+    public static function create(): array
+    {
+        $rules = self::baseRules();
+        $rules['name'] = array_merge(['required'], $rules['name']);
+        $rules['email'] = array_merge(['required', 'unique:users,email'], $rules['email']);
+        $rules['password'] = array_merge(['required'], $rules['password']);
+        return $rules;
     }
 }
